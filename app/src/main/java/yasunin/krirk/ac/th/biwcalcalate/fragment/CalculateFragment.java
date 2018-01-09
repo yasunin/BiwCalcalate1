58a531135e88899675c829924cbae7549d540814
yasunin <yasunin09@gmail.com>
2018-01-09 15:44:19 +0700

    Back Controller

--- app/src/main/java/yasunin/krirk/ac/th/biwcalcalate/fragment/CalculateFragment.java
+++ app/src/main/java/yasunin/krirk/ac/th/biwcalcalate/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package yasunin.krirk.ac.th.biwcalcalate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import yasunin.krirk.ac.th.biwcalcalate.MainActivity;
 import yasunin.krirk.ac.th.biwcalcalate.R;
 
 /**
@@ -14,6 +16,31 @@ import yasunin.krirk.ac.th.biwcalcalate.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Creat toolbar
+        creatToolbar();
+
+    }
+
+    private void creatToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarcalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getActionBar().setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
     @Nullable
     @Override
